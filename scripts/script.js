a37f8a8f601c842bd44fe6c191df43d0d74f8e57
Konstantinos Kivotos <kostasss.ryan@gmail.com>
2020-07-15 18:15:11 +0300

    Exercise 7 Javascript done

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
 window.onload = function startChange() {
 
-  var date = new Date();
-  var flex = document.getElementsByClassName("flex-container");
+  
+  changeDocumentByDay();
+  changeParagraphs();
+  onEventDblClickImages();
+  hoverImagesFlexBoxItem();
+}
+
 
   function changeDocumentByDay() {
+
+    let date = new Date();
+    var flex = document.getElementsByClassName("flex-container");
+
     if(date.getDay()==0) {
       flex[0].style.backgroundColor = "red"
     } else if (date.getDay()==1) {
       flex[0].style.backgroundColor = "blue"
-      //console.log("Hello world!");  
     } else if (date.getDay()==2) {
       flex[0].style.backgroundColor = "#fefbd8"; 
     } else if (date.getDay()==3) {
@@ -22,21 +30,59 @@ window.onload = function startChange() {
     }
   }
 
-  changeDocumentByDay();
-}
+  function changeParagraphs() {
 
-startChange();
+    var para = document.getElementsByTagName("p"); 
+    let date = new Date();
 
-function bigImages() {
-  console.log("Its happening");
-}
+    if (date.getDay()==3) {
+      for(let i = 0 ; i < para.length ; i++) {  
+        para[i].style.fontSize = "large"; 
+        para[i].style.color = "green"; 
+      }
+    }
+  }
 
 
-//Body doesnt load, srcList is undefined
+ 
 
-var images = document.getElementsByTagName('img'); 
+  function doubleClickImages() {
+    alert("Don't double click images please!");
+  }
 
-var srcList = []; 
-for(var i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
-}
+
+  function onEventDblClickImages() {
+    var photos = document.getElementsByClassName("grid-item");
+
+    for (var i = 0; i < photos.length; i++) { 
+      photos[i].addEventListener('dblclick', doubleClickImages, false);
+    }
+  }
+
+  
+
+  function hoverImagesFlexBoxItem() {
+    let photoItem = document.getElementById("flexbox-item-2");
+    photoItem.addEventListener("mouseover", ImagesFlexBoxChanging,false);
+    photoItem.addEventListener("mouseout", ImagesFlexBoxNormal,false);
+  }
+
+  function ImagesFlexBoxChanging() {
+    let photoItem = document.getElementById("flexbox-item-2");
+    photoItem.style.color = "blue";
+  }
+
+  function ImagesFlexBoxNormal () {
+    let photoItem = document.getElementById("flexbox-item-2");
+    photoItem.style.color = "black";
+  }
+
+
+
+function justALogMessage() {
+  console.log("Just a random log message to test this function");
+  }
+  justALogMessage();    
+ 
+
+
