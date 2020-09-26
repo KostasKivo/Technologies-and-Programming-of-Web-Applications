d82eab94a1ca0dc080c6723894ae690e2fac3493
Konstantinos Kivotos <kostasss.ryan@gmail.com>
2020-09-26 16:37:14 +0300

    Login Frontend and some Javascript for POST request

--- omdbapiProject/scripts/script.js
+++ omdbapiProject/scripts/script.js
@@ -12,19 +12,24 @@ function getanswer(q) {
 
 }
 
+function login(username,password) {
+	if(username==="" || password ==="") {
+		document.getElementById('username-input').value="";
+		document.getElementById('password-input').value="";
+		alert("Please fill all fields to login!");
+		return;
+	} 
 
-// getanswer function without ajax
-// function getanswer(q) {
-// 	$.get("https://www.omdbapi.com/?s="+q+"&apikey=6ff8e449", function(rawdata) {
-// 		var rawstring = JSON.stringify(rawdata);
-// 		data = JSON.parse(rawstring);
-// 		var title = data.Search[0].Title;
-// 		var year = data.Search[0].Year;
-// 		var imdburl = "https://www.imdb.com/title/" + data.Search[0].imdbID+"/";
-// 		var posterurl = data.Search[0].Poster;
-// 		// document.getElementById('answer').innerHTML="<h1>" +title+ "</h1><br><img src = "'+posterurl+'"/><br><p> Year Released: " + year+ "</p><br><p> IMDB page: <a href ="'+imdburl+'"taget ='_blank'>" + imdburl + "</a></p>";
-// 	});
-// }
+
+	$.ajax({
+		type:'POST',
+		url:'localhost:8090/api/v1/user',
+		data: JSON.stringify(document.getElementById('username-input').value),
+		function success() {
+			alert("Its successful");
+	});
+}
+
 
 function JAVAprint() {
 	console.log(data)
